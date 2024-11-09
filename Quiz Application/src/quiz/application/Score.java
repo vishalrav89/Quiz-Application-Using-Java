1f8db6c1a90eae5d873ac7b2b60ac226b9836934
vishal rav <rav62517@gmail.com>
2024-11-09 11:16:38 +0530

    first commit

---
+++ Quiz Application/src/quiz/application/Score.java
@@ -0,0 +1,44 @@
+package quiz.application;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Score extends JFrame implements ActionListener {
+
+    Score(String name, int score) {
+        setBounds(400, 150, 750, 550);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        
+        
+        JLabel heading = new JLabel("Thankyou " + name + " for playing Simple Minds");
+        heading.setBounds(120, 30, 700, 30);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        add(heading);
+        
+        JLabel lblscore = new JLabel("Your score is " + score);
+        lblscore.setBounds(270, 200, 300, 30);
+        lblscore.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        add(lblscore);
+        
+        JButton submit = new JButton("Play Again");
+        submit.setBounds(300, 270, 120, 30);
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        add(submit);
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
+
+    public static void main(String[] args) {
+        new Score("User", 0);
+    }
+}
